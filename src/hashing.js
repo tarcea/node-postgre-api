b6cc16a5f89811fc3c01aa8e3e7fa643c643c4e9
tarcea <tarcea@gmail.com>
2021-11-20 22:33:21 +0100

    login and delete routes added

--- src/hashing.js
+++ src/hashing.js
@@ -1,16 +1,5 @@
 const bcrypt = require('bcrypt');
-// const saltRounds = 10;
-// const plainText = 'secret';
-// const loginPasswordString = 'sec ret';
-// const cryptedHash = '$2b$10$usXgg75iZt13mNtchahQEOkDQCApWxUjD0DnpRS2rfowRF7oxPviq'
 
-// bcrypt.genSalt(saltRounds)
-//   .then(salt => {
-//     bcrypt.hash(plainText, salt)
-//       .then(hash => {
-//         console.log(hash);
-//       });
-//   });
 const passwordHashing = async (textPassword, saltRound) => {
   const hash = await bcrypt.hash(textPassword, saltRound);
   return hash;
@@ -19,19 +8,6 @@ const passwordDecryption = async (loginPasswordString, cryptedHash) => {
   const result = await bcrypt.compare(loginPasswordString, cryptedHash);
   return result;
 }
-
-// bcrypt.hash(plainText, saltRounds)
-//   .then(hash => {
-//     console.log(hash);
-//   });
-
-// Load hash from your database for the password.
-// bcrypt.compare(loginPasswordString, cryptedHash)
-//   .then(result => {
-//     console.log(result);
-//     // This will be either true or false, based on if the string
-//     // matches or not.
-//   });
 
 module.exports = {
   passwordHashing, passwordDecryption
