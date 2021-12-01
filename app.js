5fdbc182c46d19f9ea5d3f94c85cf167b31ede93
tarcea <tarcea@gmail.com>
2021-12-01 16:16:56 +0100

    trials with heroku

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const users = require('./src/users');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config();
+
 const port = process.env.PORT || 3001;
 const app = express();
 
 app.use('/api/users', users);
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`server listening on port ${port}`))
